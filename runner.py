b2a91ee33947d4150270d083b11e8cada7bc8f21
Jishnu Sen <jishnu1@gmail.com>
2017-08-24 19:54:07 -0700

    Make runner newbie friendly

--- runner.py
+++ runner.py
@@ -40,3 +40,27 @@ def execute(callback):
         raise e
     finally:
         _board.end_screen()
+
+def run():
+    """
+    Funtions
+    move = _refresh(_board.move) #Moves the robot forward one space!
+    turn_left = _refresh(_board.turn_left) #Turns the robot 90 degrees left
+    pick_beeper = _refresh(_board.pick_beeper) #Turns the robot 90 degrees right
+    put_beeper = _refresh(_board.put_beeper) #Takes beeper out of bag and puts it down
+    front_is_clear = _board.front_is_clear #Returns True if front is clear
+    right_is_clear = _board.right_is_clear #Returns True if right is clear
+    left_is_clear = _board.left_is_clear #Returns True if left is clear
+    beeper_is_present = _board.beeper_is_present #Returns True if there is a beeper in the bag
+    facing_north = _board.facing_north #Returns True if robot is facing north
+    facing_south = _board.facing_south #Returns True if robot is facing south
+    facing_east = _board.facing_east #Returns True if robot is facing east
+    facing_west = _board.facing_west #Returns True if robot is facing west
+    """
+    """
+    Delete pass and write your own code here!
+    """
+    pass
+
+execute(run)
+
